684004f5cf8c93d250abbad77be5f25eb10eeefe
lil69samurai <mmcgrady1tw@gmail.com>
2026-04-11 13:19:45 +0900

    feat: add global CORS configuration

---
+++ src/main/java/com/eichi/customer_api/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.eichi.customer_api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all API endpoints
+                        .allowedOrigins("*") // Allow all frontend domains
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                        .allowedHeaders("*"); // Allowed HTTP methods
+            }
+        };
+    }
+}
